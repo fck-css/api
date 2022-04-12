9dd7babb830e4a1da718f53e73234ace8f9cd759
Isa Gutierrez <isagr_kio@hotmail.com>
2022-04-12 11:03:34 +0200

    Basic config.

---
+++ config/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+// Import Middlewares
+
+// Import Controllers
+
+/* Routes
+    - Auth
+    - User
+    - Etc. */
+
+
+module.exports = router
