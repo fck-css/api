94f0485524695607de7829b8f4c00bf5f84072d7
Isa Gutierrez <isagr_kio@hotmail.com>
2022-04-12 11:32:06 +0200

    User model.

---
+++ models/User.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
+const EMAIL_REGEX = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const SALT_ROUNDS = 10
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'Email is required.'],
+            trim: true,
+            lowercase: true,
+            match: [EMAIL_REGEX, 'Please use a valid email address.']
+        },
+
+        name: {
+            type: String,
+            required: [true, 'Name is required.']
+        },
+
+        password: {
+            type: String,
+            required: [true, 'Password is required.'],
+            minlength: [8, 'The password must be at least 8 characters long.']
+
+        },
+    }, {
+        timestamps: true
+    }
+);
+
+userSchema.pre('save', function(next) {
+    if (this.isModified('password')) {
+        bcrypt.hash(this.password, SALT_ROUNDS)
+            .then(hash => {
+                this.password = hash;
+                next();
+            })
+    } else {
+        next();
+    }
+});
+
+userSchema.methods.checkPassword = function(passwordToCheck) {
+    return bcrypt.compare(passwordToCheck, this.password)
+}
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
