9b3366be23b732c5d48c584014d8282c06d770f8
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-05-07 12:46:27 +0200

    uri

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
-
-const DB_NAME = 'fck-css';
 const URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
-const DB_URI = `${URI}/${DB_NAME}`;
 
 mongoose
-    .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.info(`Successfully connected to the database ${DB_URI}.`))
+    .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.info(`Successfully connected to the database ${URI}.`))
     .catch(error => {
-        console.error(`An error ocurred when trying to connect to the database ${DB_URI}.`, error);
+        console.error(`An error ocurred when trying to connect to the database ${URI}.`, error);
         process.exit(0);
     });
 
